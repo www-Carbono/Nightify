c9bf4d98ef14804eb98a7d1381df81f8a151fdb0
Antonio <dev.antonio.gonzalez.parla@gmail.com>
2023-11-25 15:37:47 +0000

    Updated dockerfile

--- backend/constants.ts
+++ backend/constants.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 
-export const PORT = 3000
+export const PORT = 3011
 export const TEMP_FOLDER = path.join(__dirname, 'tmp')
 export const UPLOADS_FOLDER = path.join(__dirname, 'uploads')
 export const FILE_EXTENSION = '.mp3'
